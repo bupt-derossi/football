537b98371cbdb4702f11dc685508b966e1da05d9
bupt-derossi <774705788@qq.com>
2017-12-27 09:42:59 +0800

    update

--- Shell/Caipiao163Crawl.php
+++ Shell/Caipiao163Crawl.php
@@ -14,8 +14,8 @@ use Pdo\PDOModel;
 include_once __DIR__ . "/../Model/PDOModel.php";
 $objPdo = new Pdo\PDOModel();
 $handle = $objPdo->connect();
-$beginTime = date('Ymd H:i:s',strtotime(date('Ymd')));
-$endTime = date('Ymd H:i:s',strtotime(date('Ymd') . "+1 day"));
+$beginTime = date('Ymd');
+$endTime = date('Ymd',"+1 day");
 
 phpQuery::newDocumentFile($url);
 $date = date('Ymd');
